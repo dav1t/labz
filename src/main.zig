ebe841660ed393fe3243d6d4b08787414f6e44f2
Davit Mamulashvili <dav1t.mam@gmail.com>
2026-06-18 19:15:52 +0400

    simple http server

--- src/main.zig
+++ src/main.zig
@@ -1,71 +1,98 @@
 const std = @import("std");
 const Io = std.Io;
+const net = Io.net;
+const Allocator = std.mem.Allocator;
+const http = std.http;
 
-const labz = @import("labz");
+const LabzServer = struct {
+    address: net.IpAddress,
+    io: Io,
+    gpa: std.mem.Allocator,
 
-pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    tcp_server: ?net.Server,
 
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
+    pub const Options = struct { gpa: Allocator, io: Io, address: net.IpAddress };
 
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+    pub fn init(opts: Options) LabzServer {
+        return .{
+            .tcp_server = null,
+
+            .io = opts.io,
+            .gpa = opts.gpa,
+            .address = opts.address,
+        };
     }
 
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
+    pub fn start(self: *LabzServer) !void {
+        std.debug.assert(self.tcp_server == null);
+        self.tcp_server = self.address.listen(self.io, .{ .reuse_address = true }) catch |err| {
+            return err;
+        };
 
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+        try self.serve();
+        // const res = self.io.concurrent(serve, .{self}) catch |err| {
+        //     std.debug.print("unable to spawn web server thread {t}", .{err});
+        //     self.tcp_server.?.deinit(self.io);
+        //     self.tcp_server = null;
+        //     return err;
+        // };
+        // _ = res;
+    }
 
-    try labz.printAnotherMessage(stdout_writer);
+    fn serve(self: *LabzServer) !void {
+        while (true) {
+            const stream = self.tcp_server.?.accept(self.io) catch |err| {
+                std.debug.print("{any}\n", .{err});
+                return err;
+            };
 
-    try stdout_writer.flush(); // Don't forget to flush!
-}
+            const res = self.io.concurrent(handleConnection, .{ self, stream }) catch |err| {
+                std.debug.print("unable to spawn connection thread: {t}", .{err});
+                stream.close(self.io);
+                continue;
+            };
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+            _ = res;
+        }
+    }
 
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
+    fn handleConnection(self: *LabzServer, stream: net.Stream) void {
+        const io = self.io;
+        defer {
+            var copy = stream;
+            copy.close(io);
+        }
 
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+        var send_buffer: [4096]u8 = undefined;
+        var recv_buffer: [4096]u8 = undefined;
 
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
+        var connection_reader = stream.reader(io, &recv_buffer);
+        var connection_writer = stream.writer(io, &send_buffer);
+
+        var server: http.Server = .init(&connection_reader.interface, &connection_writer.interface);
+
+        while (server.reader.state == .ready) {
+            var request = server.receiveHead() catch |err| switch (err) {
+                error.HttpConnectionClosing => return,
+                else => return std.debug.print("Failed to receive http request: {t}", .{err}),
+            };
+
+            responseJson(&request, .ok, "{ \"status\": \"Okay\" }") catch |err| {
+                return std.debug.print("Failed to respond http request: {t} \n", .{err});
+            };
+        }
+    }
+
+    fn responseJson(request: *http.Server.Request, status: http.Status, body: []const u8) !void {
+        try request.respond(body, .{ .status = status, .extra_headers = &.{.{
+            .name = "content-type",
+            .value = "application/json",
+        }} });
+    }
+};
+
+pub fn main(init: std.process.Init) !void {
+    const address = try net.IpAddress.parse("127.0.0.1", 1121);
+    var server = LabzServer.init(.{ .address = address, .gpa = init.gpa, .io = init.io });
+    try server.start();
 }
