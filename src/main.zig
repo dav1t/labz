e4b7a778fc4006a804d4add8e5ecb65f5005d6e2
Davit Mamulashvili <dav1t.mam@gmail.com>
2026-06-20 20:10:08 +0400

    Add basic Router and request routing

--- src/main.zig
+++ src/main.zig
@@ -10,21 +10,55 @@ const LabzServer = struct {
     gpa: std.mem.Allocator,
 
     tcp_server: ?net.Server,
+    router: ?Router,
+    handler_context: HandlerContext,
+
+    const HandlerContext = struct { allocator: std.mem.Allocator };
+
+    const Route = struct {
+        handler: *const fn (context: *HandlerContext, request: *http.Server.Request) anyerror!void,
+        target: []const u8,
+        method: std.http.Method,
+
+        pub fn init(target: []const u8, handler: *const fn (context: *HandlerContext, request: *http.Server.Request) anyerror!void, method: http.Method) Route {
+            return .{ .target = target, .handler = handler, .method = method };
+        }
+    };
+
+    const Router = struct {
+        routes: std.StringHashMap(Route),
+
+        pub fn init(server: *LabzServer) Router {
+            return .{ .routes = .init(server.gpa) };
+        }
+
+        pub fn get(self: *Router, path: []const u8, handler: *const fn (context: *HandlerContext, request: *http.Server.Request) anyerror!void) !void {
+            const r: Route = .init(path, handler, http.Method.GET);
+
+            try self.routes.put(path, r);
+        }
+
+        pub fn deinit(self: *Router) void {
+            self.routes.deinit();
+        }
+    };
 
     pub const Options = struct { gpa: Allocator, io: Io, address: net.IpAddress };
 
     pub fn init(opts: Options) LabzServer {
-        return .{
-            .tcp_server = null,
+        return .{ .tcp_server = null, .router = null, .io = opts.io, .gpa = opts.gpa, .address = opts.address, .handler_context = .{ .allocator = opts.gpa } };
+    }
 
-            .io = opts.io,
-            .gpa = opts.gpa,
-            .address = opts.address,
-        };
+    // We can Add some options here for routes
+    pub fn initRouter(self: *LabzServer) *Router {
+        self.router = .init(self);
+
+        return &self.router.?;
     }
 
     pub fn start(self: *LabzServer) !void {
         std.debug.assert(self.tcp_server == null);
+
         self.tcp_server = self.address.listen(self.io, .{ .reuse_address = true }) catch |err| {
             return err;
         };
@@ -77,18 +111,28 @@ const LabzServer = struct {
                 else => return std.debug.print("Failed to receive http request: {t}", .{err}),
             };
 
-            const response = .{
-                .status = "ok",
-            };
-
-            const json = std.json.Stringify.valueAlloc(self.gpa, response, .{}) catch |err| switch (err) {
-                error.OutOfMemory => return std.debug.print("Failed to serialize json\n", .{}),
-            };
-
-            responseJson(&request, .ok, json) catch |err| {
-                return std.debug.print("Failed to respond http request: {t} \n", .{err});
+            // Here with errors we can swithc to proper global response
+            self.route(&request) catch |err| {
+                std.debug.print("failed to route: {t}\n", .{err});
             };
         }
+    }
+
+    fn route(self: *LabzServer, request: *http.Server.Request) !void {
+        const router = self.router orelse {
+            std.debug.print("Router not initialized\n", .{});
+            return;
+        };
+
+        const target = request.head.target;
+
+        const r = router.routes.get(target) orelse {
+            std.debug.print("Route not found: {s}\n", .{target});
+            try responseJson(request, .not_found, "{}");
+            return;
+        };
+
+        try r.handler(&self.handler_context, request);
     }
 
     fn responseJson(request: *http.Server.Request, status: http.Status, body: []const u8) !void {
@@ -97,10 +141,33 @@ const LabzServer = struct {
             .value = "application/json",
         }} });
     }
+
+    fn deinit(self: *LabzServer) void {
+        self.router.?.deinit();
+    }
 };
+
+fn getRame(context: *LabzServer.HandlerContext, request: *http.Server.Request) !void {
+    const response = .{
+        .status = "ok",
+    };
+
+    const json = std.json.Stringify.valueAlloc(context.allocator, response, .{}) catch |err| switch (err) {
+        error.OutOfMemory => return std.debug.print("Failed to serialize json\n", .{}),
+    };
+    defer context.allocator.free(json);
+
+    LabzServer.responseJson(request, .ok, json) catch |err| {
+        return std.debug.print("Failed to respond http request: {t} \n", .{err});
+    };
+}
 
 pub fn main(init: std.process.Init) !void {
     const address = try net.IpAddress.parse("127.0.0.1", 1121);
     var server = LabzServer.init(.{ .address = address, .gpa = init.gpa, .io = init.io });
+    defer server.deinit();
+    var router = server.initRouter();
+
+    try router.get("/test", getRame);
     try server.start();
 }
